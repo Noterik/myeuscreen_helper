d22703ade4d8177e2cbd1258be889b77615fe6bc
Shukri Shukriev <dark00v3@gmail.com>
2016-01-21 12:39:43 +0200

    Added ticket class

---
+++ src/org/springfield/lou/myeuscreen/util/Ticket.java
@@ -0,0 +1,104 @@
+package org.springfield.lou.myeuscreen.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+import org.springfield.fs.Fs;
+import org.springfield.fs.FsNode;
+
+public class Ticket {
+	private String ticket;
+	private String userUrl; 
+	private String ticketPropertyName;
+	
+	public String getTicketPropertyName() {
+		return ticketPropertyName;
+	}
+
+	public void setTicketPropertyName(String ticketPropertyName) {
+		this.ticketPropertyName = ticketPropertyName;
+	}
+
+	public String getTicket() {
+		return ticket;
+	}
+
+	private void setTicket(String ticket) {
+		this.ticket = ticket;
+	}
+
+	public String getUserUrl() {
+		return userUrl;
+	}
+
+	public void setUserUrl(String userUrl) {
+		this.userUrl = userUrl;
+	}
+
+	public Ticket() {
+		
+	}
+	
+	public Ticket(String userUrl, String ticketPropertyName) {
+//		String userUrl = "/domain/euscreenxl/user/" + name + "/account/default";
+		setUserUrl(userUrl);
+		setTicketPropertyName(ticketPropertyName);
+		setTicket(Ticket.generateTicketNumber());
+		System.out.println("============= INSTANTIATE A TICKET OBJECT ==============");
+		System.out.println("User url: " + userUrl);
+		System.out.println("Ticket property name: " + ticketPropertyName);
+		System.out.println("Ticket number is: " + getTicket());
+		
+		Fs.setProperty(userUrl, ticketPropertyName, getTicket());
+	}
+	
+	public boolean checkTicketExist() {
+		FsNode user = Fs.getNode(getUserUrl());
+		String ticket = user.getProperty(getTicketPropertyName());
+
+		if(ticket != null && ticket != "") {
+			System.out.println("======= TICKET IS EXISTING ========");
+			return true;
+		}
+		
+		System.out.println("======== TICKET DOES NOT EXIST =========");
+		return false;
+	}
+	
+	public static boolean checkTicketExist(String userUrl, String ticketPropertyName) {
+		FsNode user = Fs.getNode(userUrl);
+		String ticket = user.getProperty(ticketPropertyName);
+
+		if(ticket != null && ticket != "") {
+			System.out.println("======= TICKET IS EXISTING ========");
+
+			return true;
+		}
+		System.out.println("======== TICKET DOES NOT EXIST =========");
+
+		return false;
+	}
+	
+	public static boolean checkTicketExist(String userUrl, String ticketPropertyName, String ticketNumber) {
+		FsNode user = Fs.getNode(userUrl);
+		String ticket = user.getProperty(ticketPropertyName);
+
+		if(ticket != null && ticket != "" && ticket == ticketNumber) {
+			System.out.println("======= TICKET IS EXISTING ========");
+
+			return true;
+		}
+		System.out.println("======== TICKET DOES NOT EXIST =========");
+
+		return false;
+	}
+
+	public static String generateTicketNumber() {
+		Random random = new Random();
+		DateFormat date = new SimpleDateFormat("yyyyMMdd");
+		String ticketNumber = date.format(new Date()) + String.valueOf(random.nextInt(999999999));
+		return ticketNumber;
+	}
+}
